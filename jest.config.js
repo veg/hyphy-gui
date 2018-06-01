a107443cb30f9faaf5ebbe97aa4e0db7d5e4a30e
rdvelazquez <ryan.velazquez0001@temple.edu>
2018-06-01 06:41:22 -0400

    tooling: code-coverage and prettier

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  "moduleNameMapper": {
+  moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
-    "Home": "<rootDir>/src/components/home.jsx",
-    "NavBar": "<rootDir>/src/components/navbar.jsx"
-  }
+    Home: "<rootDir>/src/components/home.jsx",
+    NavBar: "<rootDir>/src/components/navbar.jsx"
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/hyphy-vision/"],
+  collectCoverageFrom: [
+    "**/src/**.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/hyphy-vision/**"
+  ]
 };
