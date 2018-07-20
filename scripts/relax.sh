f6f1e277a81632fce964da9d6479e1a28f3a962a
rdvelazquez <ryan.velazquez0001@temple.edu>
2018-07-20 12:25:36 -0400

    update relax script

--- scripts/relax.sh
+++ scripts/relax.sh
@@ -6,20 +6,4 @@ TREE_PATH=$3
 GENETIC_CODE=$4
 ANALYSIS_TYPE=$5
 
-GETCOUNT=$HYPHY_DIR/../src/helpers/getAnnotatedCount.bf
-
-echo "hello from bash"
-test=3
-echo $test
-
-#count=(echo '(echo '$TREE_FN') | '$HYPHY' '$GETCOUNT'' 2> /dev/null)
-#echo $count
-
-#if[ $count -eq 2]
-#then
-#  echo "then block"
-#else
-#  echo "else block"
-#fi
-
-#(echo $GENETIC_CODE; echo $DATA_PATH; echo $TREE_PATH; echo 3; echo 1; echo $ANALYSIS_TYPE;) | $HYPHY_DIR/HYPHYMP LIBPATH=$LIB_PATH $BATCH_FILE_PATH
+(echo $GENETIC_CODE; echo $DATA_PATH; echo $TREE_PATH; echo 3; echo 2; echo $ANALYSIS_TYPE;) | $HYPHY_DIR/HYPHYMP LIBPATH=$LIB_PATH $BATCH_FILE_PATH
